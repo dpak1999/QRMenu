6b34a951fb4474f065a7469a9520c4713c9446d9
dpak1999 <deepakkumardash313@gmail.com>
2021-10-16 19:51:44 +0530

    add : home login and app pages

---
+++ qrmenu_frontend/src/router/App.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={() => <Home />} />
+        <Route exact path="/login" component={() => <Login />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
